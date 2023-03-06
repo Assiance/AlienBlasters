78c9b30f6f4b3480e72e6fb6be6bd8e913e59981
Bradford Hill <bradford.foxworth-hill>
2023-03-06 01:07:31 -0500

    added frog prefab

--- Assets/Scripts/Frog.cs
+++ Assets/Scripts/Frog.cs
@@ -8,27 +8,37 @@ public class Frog : MonoBehaviour
     SpriteRenderer _spriteRenderer;
     
     Sprite _defaultSprite;
+    int _jumpsRemaining;
     
     [SerializeField] float _jumpDelay = 3;
     [SerializeField] Vector2 _jumpForce;
     [SerializeField] Sprite _jumpSprite;
-    
+    [SerializeField] int _jumps = 2;
+
     void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _defaultSprite = _spriteRenderer.sprite;
 
+        _jumpsRemaining = _jumps;
         InvokeRepeating(nameof(Jump), _jumpDelay, _jumpDelay);
     }
 
     void Jump()
     {
         _rb.AddForce(_jumpForce);
+        _spriteRenderer.flipX = _jumpForce.x > 0;
         _spriteRenderer.sprite = _jumpSprite;
-        
-        _jumpForce *= new Vector2(-1, 1);
-        _spriteRenderer.flipX = !_spriteRenderer.flipX;
+
+        _jumpsRemaining--;
+
+        if (_jumpsRemaining <= 0)
+        {
+            _jumpForce *= new Vector2(-1, 1);
+
+            _jumpsRemaining = _jumps;
+        }
     }
 
     void OnCollisionEnter2D(Collision2D other)
