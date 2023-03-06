223717f98e1ee25ce88570d1b21e2c9168c78fae
Bradford Hill <bradford.foxworth-hill>
2023-03-06 00:43:09 -0500

    add frog jump

--- Assets/Scripts/Frog.cs
+++ Assets/Scripts/Frog.cs
@@ -7,18 +7,32 @@ public class Frog : MonoBehaviour
     Rigidbody2D _rb;
     SpriteRenderer _spriteRenderer;
     
+    Sprite _defaultSprite;
+    
     [SerializeField] float _jumpDelay = 3;
-    [SerializeField] Vector2 _force;
-
+    [SerializeField] Vector2 _jumpForce;
+    [SerializeField] Sprite _jumpSprite;
+    
     void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        _defaultSprite = _spriteRenderer.sprite;
+
         InvokeRepeating(nameof(Jump), _jumpDelay, _jumpDelay);
     }
 
     void Jump()
     {
-        _rb.AddForce(_force);
+        _rb.AddForce(_jumpForce);
+        _spriteRenderer.sprite = _jumpSprite;
+        
+        _jumpForce *= new Vector2(-1, 1);
+        _spriteRenderer.flipX = !_spriteRenderer.flipX;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        _spriteRenderer.sprite = _defaultSprite;
     }
 }
