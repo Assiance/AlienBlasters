8b65ebb3e58f7b0fd19efebb9ccaaa7938f0be8b
Bradford Hill <bradford.foxworth-hill>
2023-03-08 01:32:55 -0500

    added slippy ground

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,28 +3,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    float _jumpEndTime;
     [SerializeField] float _maxHorizontalSpeed = 5;
     [SerializeField] float _jumpVelocity = 5;
     [SerializeField] float _jumpDuration = 0.5f;
     [SerializeField] Sprite _jumpSprite;
-    [SerializeField] private LayerMask _layerMask;
-    [SerializeField] private float _footOffset = 0.5f;
-    [SerializeField] private int _jumpsRemaining;
-    [SerializeField] float _acceleration = 10;
+    [SerializeField] LayerMask _layerMask;
+    [SerializeField] float _footOffset = 0.5f;
+    [SerializeField] float _groundAcceleration = 10;
+    [SerializeField] float _snowAcceleration = 1;
 
     public bool IsGrounded;
-    
-    SpriteRenderer _spriteRenderer;
-    Animator _animator;
-    private float _horizontal;
-    private AudioSource _audioSource;
+    public bool IsOnSnow;
 
-    private void Awake()
+    Animator _animator;
+    SpriteRenderer _spriteRenderer;
+    AudioSource _audioSource;
+    Rigidbody2D _rb;
+
+    float _horizontal;
+    int _jumpsRemaining;
+    float _jumpEndTime;
+
+
+    void Awake()
     {
-        _spriteRenderer = GetComponent<SpriteRenderer>();
         _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
         _audioSource = GetComponent<AudioSource>();
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     void OnDrawGizmos()
@@ -35,11 +41,11 @@ public class Player : MonoBehaviour
         Vector2 origin = new Vector2(transform.position.x, transform.position.y - spriteRenderer.bounds.extents.y);
         Gizmos.DrawLine(origin, origin + Vector2.down * 0.1f);
 
-        //Draw Left Foot
+        // Draw Left Foot
         origin = new Vector2(transform.position.x - _footOffset, transform.position.y - spriteRenderer.bounds.extents.y);
         Gizmos.DrawLine(origin, origin + Vector2.down * 0.1f);
 
-        //Draw Right Foot
+        // Draw Right Foot
         origin = new Vector2(transform.position.x + _footOffset, transform.position.y - spriteRenderer.bounds.extents.y);
         Gizmos.DrawLine(origin, origin + Vector2.down * 0.1f);
     }
@@ -50,16 +56,15 @@ public class Player : MonoBehaviour
         UpdateGrounding();
 
         var horizontalInput = Input.GetAxis("Horizontal");
-        Debug.Log(_horizontal);
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        var vertical = rb.velocity.y;
+
+        var vertical = _rb.velocity.y;
 
         if (Input.GetButtonDown("Fire1") && _jumpsRemaining > 0)
         {
             _jumpEndTime = Time.time + _jumpDuration;
             _jumpsRemaining--;
 
-            _audioSource.pitch = _jumpsRemaining > 0 ? 1f : 1.5f;
+            _audioSource.pitch = _jumpsRemaining > 0 ? 1 : 1.2f;
             _audioSource.Play();
         }
 
@@ -67,38 +72,50 @@ public class Player : MonoBehaviour
             vertical = _jumpVelocity;
 
         var desiredHorizontal = horizontalInput * _maxHorizontalSpeed;
-        _horizontal = Mathf.Lerp(_horizontal, desiredHorizontal, Time.deltaTime * _acceleration);
-        rb.velocity = new Vector2(_horizontal, vertical);
+        var acceleration = IsOnSnow ? _snowAcceleration : _groundAcceleration;
+
+        _horizontal = Mathf.Lerp(_horizontal, desiredHorizontal, Time.deltaTime * acceleration);
+        _rb.velocity = new Vector2(_horizontal, vertical);
         UpdateSprite();
     }
 
-    private void UpdateGrounding()
+    void UpdateGrounding()
     {
         IsGrounded = false;
+        IsOnSnow = false;
 
-        //Check Left
+        // Check center
         Vector2 origin = new Vector2(transform.position.x, transform.position.y - _spriteRenderer.bounds.extents.y);
-        var hit = Physics2D.Raycast(origin, Vector2.down, 0.1f, _layerMask);
+        RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down, 0.1f, _layerMask);
         if (hit.collider)
+        {
             IsGrounded = true;
+            IsOnSnow = hit.collider.CompareTag("Snow");
+        }
 
-        //Check Right
+        // Check left
         origin = new Vector2(transform.position.x - _footOffset, transform.position.y - _spriteRenderer.bounds.extents.y);
         hit = Physics2D.Raycast(origin, Vector2.down, 0.1f, _layerMask);
         if (hit.collider)
+        {
             IsGrounded = true;
+            IsOnSnow = hit.collider.CompareTag("Snow");
+        }
 
-        //Check Left
+        // Check right
         origin = new Vector2(transform.position.x + _footOffset, transform.position.y - _spriteRenderer.bounds.extents.y);
         hit = Physics2D.Raycast(origin, Vector2.down, 0.1f, _layerMask);
         if (hit.collider)
+        {
             IsGrounded = true;
+            IsOnSnow = hit.collider.CompareTag("Snow");
+        }
 
         if (IsGrounded && GetComponent<Rigidbody2D>().velocity.y <= 0)
             _jumpsRemaining = 2;
     }
 
-    private void UpdateSprite()
+    void UpdateSprite()
     {
         _animator.SetBool("IsGrounded", IsGrounded);
         _animator.SetFloat("HorizontalSpeed", Math.Abs(_horizontal));
