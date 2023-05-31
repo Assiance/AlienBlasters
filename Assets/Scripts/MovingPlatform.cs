de8f05c3595953db9d577d701a09ee8022749949
Bradford Hill <bradford.foxworth-hill>
2023-05-31 00:29:52 -0400

    added manually controlled moving platform

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] Vector3 _position1;
+    [SerializeField] Vector3 _position2;
+    [Range(0, 1)]
+    [SerializeField] float _percentAcross;
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(_position1, _position2, _percentAcross);
+    }
+}
