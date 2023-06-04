aaa968d46b45240d1b055c7113c86fc824799be9
Bradford Hill <bradford.foxworth-hill>
2023-06-04 02:06:39 -0400

    ladybug turns around at walls

--- Assets/Scripts/Ladybug.cs
+++ Assets/Scripts/Ladybug.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class Ladybug : MonoBehaviour
 {
     [SerializeField] float _speed = 1f;
-
+    [SerializeField] float _raycastDistance = 0.2f;
+    
     private Vector2 _direction = Vector2.left;
 
     SpriteRenderer _spriteRenderer;
@@ -21,6 +22,21 @@ public class Ladybug : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector2 offset = _direction * _collider.bounds.extents.x;
+        Vector2 origin = (Vector2)transform.position + offset;
+
+        var hits = Physics2D.RaycastAll(origin, _direction, _raycastDistance);
+
+        foreach (var hit in hits)
+        {
+            if (hit.collider != null && hit.collider.gameObject != gameObject)
+            {
+                _direction *= -1;
+                _spriteRenderer.flipX = _direction == Vector2.right;
+                break;
+            }
+        }
+        
         _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
     }
 }
