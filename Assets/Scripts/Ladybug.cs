18c5adf81cb3ba1ac293e3103c72ccaa53e01834
Bradford Hill <bradford.foxworth-hill>
2023-06-04 02:31:48 -0400

    ladybug no longer walks off edges

--- Assets/Scripts/Ladybug.cs
+++ Assets/Scripts/Ladybug.cs
@@ -1,4 +1,3 @@
-using System.Numerics;
 using UnityEngine;
 using Vector2 = UnityEngine.Vector2;
 
@@ -6,14 +5,13 @@ public class Ladybug : MonoBehaviour
 {
     [SerializeField] float _speed = 1f;
     [SerializeField] float _raycastDistance = 0.2f;
-    
+
     private Vector2 _direction = Vector2.left;
 
     SpriteRenderer _spriteRenderer;
     Collider2D _collider;
     Rigidbody2D _rigidbody;
 
-    // Start is called before the first frame update
     void Awake()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
@@ -24,34 +22,53 @@ public class Ladybug : MonoBehaviour
     void OnDrawGizmos()
     {
         var collider = GetComponent<Collider2D>();
-        
+
         Vector2 offset = _direction * collider.bounds.extents.x;
         Vector2 origin = (Vector2)transform.position + offset;
         Gizmos.color = Color.red;
         Gizmos.DrawLine(origin, origin + _direction * _raycastDistance);
 
+        var downOrigin = GetDownRayPosition(collider);
         var bounds = collider.bounds;
 
-        if (_direction == Vector2.left)
-        {
-            Vector2 bottomLeft = new Vector2(bounds.center.x - bounds.extents.x, bounds.center.y - bounds.extents.y);
-            Gizmos.DrawLine(bottomLeft, bottomLeft + Vector2.down * _raycastDistance);
-        }
-        else
-        {
-            Vector2 bottomRight = new Vector2(bounds.center.x + bounds.extents.x, bounds.center.y - bounds.extents.y);
-            Gizmos.DrawLine(bottomRight, bottomRight + Vector2.down * _raycastDistance);
-        }
+        Gizmos.DrawLine(downOrigin, downOrigin + Vector2.down * _raycastDistance);
     }
 
-    // Update is called once per frame
+    private Vector2 GetDownRayPosition(Collider2D collider)
+    {
+        var bounds = collider.bounds;
+        if (_direction == Vector2.left)
+            return new Vector2(bounds.center.x - bounds.extents.x, bounds.center.y - bounds.extents.y);
+        else
+            return new Vector2(bounds.center.x + bounds.extents.x, bounds.center.y - bounds.extents.y);
+    }
+
     void Update()
     {
         Vector2 offset = _direction * _collider.bounds.extents.x;
         Vector2 origin = (Vector2)transform.position + offset;
 
-        var hits = Physics2D.RaycastAll(origin, _direction, _raycastDistance);
+        var canContinueWalking = false;
+        var downOrigin = GetDownRayPosition(_collider);
+        var downHits = Physics2D.RaycastAll(downOrigin, Vector2.down, _raycastDistance);
 
+        foreach (var hit in downHits)
+        {
+            if (hit.collider != null && hit.collider.gameObject != gameObject)
+            {
+                canContinueWalking = true;
+                break;
+            }
+        }
+
+        if (canContinueWalking == false)
+        {
+            _direction *= -1;
+            _spriteRenderer.flipX = _direction == Vector2.right;
+            return;
+        }
+
+        var hits = Physics2D.RaycastAll(origin, _direction, _raycastDistance);
         foreach (var hit in hits)
         {
             if (hit.collider != null && hit.collider.gameObject != gameObject)
@@ -61,7 +78,7 @@ public class Ladybug : MonoBehaviour
                 break;
             }
         }
-        
+
         _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
     }
 }
