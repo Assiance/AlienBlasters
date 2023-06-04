82fa98e10e28ee1fd86d1aa29d525d724c37c027
Bradford Hill <bradford.foxworth-hill>
2023-06-04 00:16:52 -0400

    added laser hit detection

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
+    [SerializeField] Vector2 _direction = Vector2.left;
+    [SerializeField] float _distance = 10f;
+
     LineRenderer _lineRenderer;
     bool _isOn;
 
@@ -15,5 +18,21 @@ public class Laser : MonoBehaviour
     {
         _isOn = state;
         _lineRenderer.enabled = state;
+    }
+
+    void Update()
+    {
+        if (!_isOn)
+            return;
+        
+        var endPoint = (Vector2)transform.position + (_direction * _distance);
+
+        var firstThing = Physics2D.Raycast(transform.position, _direction, _distance);
+        if (firstThing.collider)
+        {
+            endPoint = firstThing.point;
+        }
+
+        _lineRenderer.SetPosition(1, endPoint);
     }
 }
