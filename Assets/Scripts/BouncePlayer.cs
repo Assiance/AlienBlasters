8eb3cc84437f6b6a5368f7b7e1458ffbce29cdb7
Bradford Hill <bradford.foxworth-hill>
2023-06-14 00:19:25 -0400

    added bounce player script to ladybug and spring

---
+++ Assets/Scripts/BouncePlayer.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class BouncePlayer : MonoBehaviour
+{
+    [SerializeField] bool _onlyFromTop = false;
+    [SerializeField] float _bounciness = 200f;
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (_onlyFromTop && Vector2.Dot(other.contacts[0].normal, Vector2.down) < 0.5f)
+            return;
+
+        if (other.collider.CompareTag("Player"))
+        {
+            var player = other.gameObject.GetComponent<Player>();
+            if (player)
+                player.Bounce(other.contacts[0].normal, _bounciness);
+        }
+    }
+}
