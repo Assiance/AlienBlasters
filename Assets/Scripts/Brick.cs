636cc763db0691c579ceb6cd606ad0263bcdcd7c
Bradford Foxworth-Hill <bradford.foxworthhill@gmail.com>
2024-04-18 18:37:34 -0400

    added brick to be destroyed when hit from below

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        var player = collision.gameObject.GetComponent<Player>();
+        if (player == null)
+            return;
+
+        Vector2 normal = collision.GetContact(0).normal;
+        float dot = Vector2.Dot(normal, Vector2.up);
+        Debug.Log(dot);
+
+        if (dot > 0.5f)
+            Destroy(gameObject);
+    }
+}
