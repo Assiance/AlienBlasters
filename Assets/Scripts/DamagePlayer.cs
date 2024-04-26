f2bd3593c0a8c3b1a9989109bd0ad4dff5abcd34
Bradford Foxworth-Hill <bradford.foxworthhill@gmail.com>
2024-04-26 16:11:31 -0400

    Made the Spikes into a DamagePlayer script and gave it an option to ignore hits from the top

--- Assets/Scripts/DamagePlayer.cs
+++ Assets/Scripts/DamagePlayer.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class Spikes : MonoBehaviour
+public class DamagePlayer : MonoBehaviour
 {
+    [SerializeField] bool _ignoreFromTop;
+
     void OnCollisionEnter2D(Collision2D other)
     {
+        var hitFromTop = Vector2.Dot(other.GetContact(0).normal, Vector2.down) > 0.5f;
+        if (_ignoreFromTop && hitFromTop)
+            return;
+
         if (other.collider.CompareTag("Player"))
         {
             var player = other.collider.GetComponent<Player>();
