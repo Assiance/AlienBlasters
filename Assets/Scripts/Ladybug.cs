38d0d0d9c830a3b0e086c53d1782cb6256b889a2
Bradford Foxworth-Hill <bradford.foxworthhill@gmail.com>
2024-04-26 15:19:56 -0400

    ladybug deals damage to players

--- Assets/Scripts/Ladybug.cs
+++ Assets/Scripts/Ladybug.cs
@@ -4,6 +4,8 @@ public class Ladybug : MonoBehaviour, ITakeLaserDamage
 {
     [SerializeField] float _speed = 1f;
     [SerializeField] float _raycastDistance = 0.2f;
+    [SerializeField] LayerMask _forwardRaycastLayerMask;
+
     Vector2 _direction = Vector2.left;
 
     SpriteRenderer _spriteRenderer;
@@ -41,9 +43,35 @@ public class Ladybug : MonoBehaviour, ITakeLaserDamage
 
     void Update()
     {
+        CheckGroundInFront();
+        CheckInFront();
+
+        _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
+    }
+
+    public void TakeLaserDamage()
+    {
+        _rigidbody.velocity = Vector2.zero;
+    }
+
+    void CheckInFront()
+    {
         var offset = _direction * _collider.bounds.extents.x;
         var origin = (Vector2)transform.position + offset;
 
+        var hits = Physics2D.RaycastAll(origin, _direction, _raycastDistance, _forwardRaycastLayerMask);
+        foreach (var hit in hits)
+        {
+            if (hit.collider != null && hit.collider.gameObject != gameObject)
+            {
+                _direction *= -1;
+                _spriteRenderer.flipX = _direction == Vector2.right;
+            }
+        }
+    }
+
+    void CheckGroundInFront()
+    {
         var canContinueWalking = false;
         var downOrigin = GetDownRayPosition(_collider);
         var downHits = Physics2D.RaycastAll(downOrigin, Vector2.down, _raycastDistance);
@@ -63,22 +91,5 @@ public class Ladybug : MonoBehaviour, ITakeLaserDamage
             _spriteRenderer.flipX = _direction == Vector2.right;
             return;
         }
-
-        var hits = Physics2D.RaycastAll(origin, _direction, _raycastDistance);
-        foreach (var hit in hits)
-        {
-            if (hit.collider != null && hit.collider.gameObject != gameObject)
-            {
-                _direction *= -1;
-                _spriteRenderer.flipX = _direction == Vector2.right;
-            }
-        }
-
-        _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
-    }
-
-    public void TakeLaserDamage()
-    {
-        _rigidbody.velocity = Vector2.zero;
     }
 }
