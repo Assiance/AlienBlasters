32059d5497aec648c12f707dbe23f5be872f10c0
Bradford Foxworth-Hill <bradford.foxworthhill@gmail.com>
2024-05-04 02:34:53 -0400

    blaster animates when firing

--- Assets/Scripts/Blaster.cs
+++ Assets/Scripts/Blaster.cs
@@ -7,27 +7,34 @@ public class Blaster : MonoBehaviour
 
     Player _player;
     PlayerInput _playerInput;
+    Animator _animator;
 
     // Start is called before the first frame update
     void Awake()
     {
+        _animator = GetComponentInChildren<Animator>();
         _player = GetComponent<Player>();
         _playerInput = GetComponent<PlayerInput>();
-        //_playerInput.actions["Fire"].performed += TryFire;
+        _playerInput.actions["Fire"].performed += TryFire;
     }
 
     void TryFire(InputAction.CallbackContext obj)
     {
+        Fire();
+    }
+
+    private void Fire()
+    {
         var shot = PoolManager.Instance.GetBlasterShot();
         shot.Launch(_player.Direction, _firePoint.position);
+        _animator.SetTrigger("Fire");
     }
 
     void Update()
     {
         if (_playerInput.actions["Fire"].ReadValue<float>() > 0)
         {
-            var shot = PoolManager.Instance.GetBlasterShot();
-            shot.Launch(_player.Direction, _firePoint.position);
+           // Fire();
         }
     }
 }
