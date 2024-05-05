f763bb44a9c13701cec9e05a7397ac02ab3c5031
Bradford Foxworth-Hill <bradford.foxworthhill@gmail.com>
2024-05-05 16:14:02 -0400

    added player inventory item switching

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,24 +7,56 @@ public class PlayerInventory : MonoBehaviour
     public Transform ItemPoint;
 
     PlayerInput _playerInput;
-    Key _quippedKey;
+
+    Key EquippedKey
+    {
+        get
+        {
+            return _items.Count >= _currentItemIndex ? _items[_currentItemIndex] : null;
+        }
+    }
+
+    List<Key> _items = new List<Key>();
+    int _currentItemIndex;
 
     void Awake()
     {
         _playerInput = GetComponent<PlayerInput>();
         _playerInput.actions["Fire"].performed += UseEquippedItem;
+        _playerInput.actions["EquipNext"].performed += EquipNext;
+    }
+
+    void EquipNext(InputAction.CallbackContext obj)
+    {
+        _currentItemIndex++;
+        if (_currentItemIndex >= _items.Count)
+            _currentItemIndex = 0;
+
+        ToggleEquippedItem();
+    }
+
+    private void ToggleEquippedItem()
+    {
+        for (int i = 0; i < _items.Count; i++)
+        {
+            _items[i].gameObject.SetActive(i == _currentItemIndex);
+        }
     }
 
     private void UseEquippedItem(InputAction.CallbackContext obj)
     {
-        if (_quippedKey)
-            _quippedKey.Use();
+        if (EquippedKey)
+            EquippedKey.Use();
     }
 
     public void Pickup(Key key)
     {
         key.transform.SetParent(ItemPoint);
         key.transform.localPosition = Vector3.zero;
-        _quippedKey = key;
+        
+        _items.Add(key);
+        _currentItemIndex = _items.Count - 1;
+
+        ToggleEquippedItem();
     }
 }
