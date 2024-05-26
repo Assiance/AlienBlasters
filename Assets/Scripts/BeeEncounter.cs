0da9edc3b726b79aa0d6cbacc5db1c7ebb359a8d
Bradford Foxworth-Hill <bradford.foxworthhill@gmail.com>
2024-05-25 22:11:21 -0400

    Added player damage and some lightning control

--- Assets/Scripts/BeeEncounter.cs
+++ Assets/Scripts/BeeEncounter.cs
@@ -5,7 +5,19 @@ using UnityEngine;
 public class BeeEncounter : MonoBehaviour
 {
     [SerializeField] List<Transform> _lightnings;
+    [SerializeField] float _delayBeforeDamage = 1.5f;
+    [SerializeField] float _lightningAnimationTime = 2f;
     [SerializeField] float _delayBetweenLightning = 1f;
+    [SerializeField] float _lightningRadius = 1f;
+    [SerializeField] LayerMask _playerLayer;
+
+    Collider2D[] _playerHitResults =  new Collider2D[10];
+
+    void OnValidate()
+    {
+        if (_lightningAnimationTime <= _delayBeforeDamage)
+            _delayBeforeDamage = _lightningAnimationTime;
+    }
 
     void OnEnable()
     {
@@ -25,8 +37,23 @@ public class BeeEncounter : MonoBehaviour
 
             int index = Random.Range(0, _lightnings.Count);
             _lightnings[index].gameObject.SetActive(true);
+            yield return new WaitForSeconds(_delayBeforeDamage);
 
+            DamagePlayersInRange(_lightnings[index]);
+            yield return new WaitForSeconds(_lightningAnimationTime - _delayBeforeDamage);
+            
+            _lightnings[index].gameObject.SetActive(false);
             yield return new WaitForSeconds(_delayBetweenLightning);
+        }
+    }
+
+    void DamagePlayersInRange(Transform lightning)
+    {
+        var hits = Physics2D.OverlapCircleNonAlloc(lightning.position, _lightningRadius, _playerHitResults, _playerLayer);
+        for (int i = 0; i < hits; i++)
+        {
+            var player = _playerHitResults[i].GetComponent<Player>();
+            player?.TakeDamage(Vector3.zero);
         }
     }
 }
