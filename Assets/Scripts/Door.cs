b12d18132f05cd2a468462069025f4bc03923b93
Bradford Foxworth-Hill <bradford.foxworthhill@gmail.com>
2024-07-04 18:23:08 -0400

    Added the working interactable door

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -25,5 +26,34 @@ public class Door : MonoBehaviour
         _opened2.SetActive(false);
         _closed1.SetActive(true);
         _closed2.SetActive(true);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        var playerInteractionController = other.GetComponent<PlayerInteractionController>();
+        if (playerInteractionController)
+        {
+            playerInteractionController.Add(this);
+        }
+    }
+    
+    void OnTriggerExit2D(Collider2D other)
+    {
+        var playerInteractionController = other.GetComponent<PlayerInteractionController>();
+        if (playerInteractionController)
+        {
+            playerInteractionController.Remove(this);
+        }
+    }
+
+    public void Interact(PlayerInteractionController playerInteractionController)
+    {
+        var distance1 = Vector2.Distance(playerInteractionController.transform.position, _opened1.transform.position);
+        var distance2 = Vector2.Distance(playerInteractionController.transform.position, _opened2.transform.position);
+        
+        if (distance1 < distance2)
+            playerInteractionController.transform.position = _opened2.gameObject.transform.position;
+        else
+            playerInteractionController.transform.position = _opened1.gameObject.transform.position;
     }
 }
